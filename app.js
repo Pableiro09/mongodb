58f699b9a88bf5fd3f3a4a62b8dea13fe2a115e8
Pableiro09 <prs0011@alu.medac.es>
2024-01-19 10:09:54 +0100

    mongodb terminado

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
+const User  = require('./public/user');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -13,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-mongoose.connect('mongodb://localhost:3000/', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://prs0011:6JMpW5V4QnnbhQz1@login.tdcfzxp.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Conexión exitosa a la base de datos');
     // Resto de tu código...
@@ -22,9 +23,43 @@ mongoose.connect('mongodb://localhost:3000/', { useNewUrlParser: true, useUnifie
     console.error('Error de conexión a la base de datos:', error.message);
   });
 
-app.get('/', (req, res)=>{
+  app.post('/register', async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const user = new User({ username, password });
+  
+      await user.save();
+      res.status(200).send('Usuario registrado');
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Fallo al registrar el usuario');
+    }
+  });
+  
+  app.post('/authenticate', async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+  
+      if (!user) {
+        res.status(500).send('Usuario no encontrado');
+        return;
+      }
+  
+      const result = await user.isCorrectPassword(password);
+  
+      if (result) {
+        res.status(200).send('Usuario logueado correctamente');
+      } else {
+        res.status(500).send('Usuario y/o contraseña incorrecta');
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Error al loguear el usuario');
+    }
+  });
+  
 
-});
 app.listen(3000,()=>{
     console.log('server started');
 })
